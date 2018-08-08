ce388bf5c4d495d1fbadb035063a6dbe6ef07bab
Elie Harfouche <ehfche@gmail.com>
2018-08-08 15:59:14 -0400

    1

---
+++ app/models/restaurant.rb
@@ -0,0 +1,8 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  validates :name, uniqueness: true, presence: true
+  validates :category, presence: true, inclusion: { in: %w(chinese italian japanese french belgian) }
+  validates :address, presence: true
+end
+
