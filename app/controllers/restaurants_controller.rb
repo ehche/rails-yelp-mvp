764fed7b44803900a4249b5398d23431d2576421
Elie Harfouche <ehfche@gmail.com>
2018-08-08 17:41:57 -0400

    3

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,6 +6,7 @@ class RestaurantsController < ApplicationController
   end
 
   def show
+    @restaurant = Restaurant.find{id}
   end
 
   def new
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+
+Restaurant.create!(
+  name: "Red Rooster",
+  address: "123 Fake St.",
+  phone_number: "123-456-7890",
+  category: "belgian"
+)
+
+new_review = Review.new(
+  content: "very french"
+  rating: 5
+)
+
+
+new_review.restaurant = guillaume
+new_review.save!
